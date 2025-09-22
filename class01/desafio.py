bb228afb3d98340653f292854d74c517485e803d
Leticia Figueira <jlcfigueira@gmail.com>
2025-09-22 10:53:15 -0300

    Adiciona o desafio da aula 01

---
+++ class01/desafio.py
@@ -0,0 +1,12 @@
+#/bin/python3
+
+# ⭐ DESAFIO: Crie um primeiro programa, em seu editor de texto de preferência, 
+# que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+# Crie um arquivo, nomeie, salve em sua pasta de exercícios
+# Salve a informação de nome fornecida em uma variável "nome"
+# Utilize a função "input" para fazer perguntas ao usuário
+# Coloque a sua mensagem dentro de aspas e parênteses
+# Utilize a função "print" para que o interpretador escreva na tela a mensagem
+
+nome = input("Qual é o seu nome? ")
+print("Seja bem-vindo(a), " + nome + "!")
